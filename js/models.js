728cb371f5baf8244fbfbe5377bcc9e62b2782fd
rserva <rserva@udel.edu>
2016-03-08 13:45:13 +0000

    updated models to make more sense. initEnemies is TODO

--- js/models.js
+++ js/models.js
@@ -1,81 +1,53 @@
 // THIS FILE DEALS WITH THE CLASSES FOR THE GAME OBJECTS
 // THIS AREA ADDRESSES THE TOWER
-function Tower(name,firerate,cost){
+function Tower(name, fireRate, damage, cost){
     this.name = name;
-    this.rate = firerate;
+    this.fireRate = fireRate;
+    this.damage = damage;
     this.cost = cost;
-    this.sell = this.updateSellValue();
-    this.damage = this.initDamageValue();
 };
 
-Tower.prototype.updateCost = function(newCost){
-  this.cost += newCost;
-  this.updateSellValue();
+Tower.prototype.upgrade = function(cost, fireRate, damage){
+  this.cost += cost;
+  this.fireRate = fireRate;
+  this.damage = damage;
 };
 
-Tower.prototype.updateSellValue = function(){
-  this.sell = Math.floor((this.cost / 2));  
-};
-
-Tower.prototype.updateFireRate = function(newRate){
-  this.rate = newRate;  
-};
-
-Tower.prototype.initDamageValue = function(){
-     // The 10 is a placeholder value
-    // This will be changed to allow for Type/name to determine damage value
-    return name * 10;
-};
-
-Tower.prototype.updateDamage = function(newDamage){
-  this.damage = newDamage;  
+Tower.prototype.sell = function(){
+  return Math.floor(this.cost * .8);  
 };
 
 // THIS AREA ADDRESSES THE ENEMY(FBI)
-function Enemy(type){
+function Enemy(type, health, armor){
     this.type = type;
-    this.health = this.initHealth();
-    this.damage = this.setDamage(type);
+    this.health = health;
+    this.armor = armor;
 };
     
-Enemy.prototype.initHealth = function(){
-    this.health = 100;
-}
+Enemy.prototype.damage = function(damage){
+    this.health -= damage*((100-this.armor)/100);  
+    return this.health <= 0;
+};
 
-Enemy.prototype.setDamage = function(enemType){
-    // The 10 is a placeholder value
-    // This will be changed to allow for enemy type to determine damage value
-    return enemType * 10;
-}    
-    
-Enemy.prototype.updateHealth = function(healthdamage){
-    this.health -= healthdamage;  
-};
 
 // THIS AREA ADDRESSES THE GAME
-function Game(level, wave){
-  this.level = level;
-  this.wave = wave;
-  this.credits = this.initCredit(level,wave);
-  this.Enemies = this.initEnemies(level,wave);
+function Game(){
+  this.wave = 0;
+  this.money = 500;
+  this.health = 100;
+  this.Enemies = this.initEnemies(wave);
 };
 
-Game.prototype.initEnemies = function(level, wave){
-  var enemies = [];
-  
-  enemies.push(anenem);
-  
-  return enemies;
+Game.prototype.nextWave = function(){
+  this.wave ++;
+  this.initEnemies();
 };
 
-Game.prototype.initCredit = function(level,wave){
-    // The 10 is a placeholder value
-    // This will be changed to allow for enemy type to determine damage value
-    // (level,wave)
-    return  250;
+Game.prototype.initEnemies = function(){
+  this.enemies = [];//Will be changed to dynamic function
 };
 
-Game.prototype.upgradeCredits = function(spent){
+Game.prototype.spendCredits = function(spent){
   this.credits -= spent;  
 };
 
@@ -83,15 +55,9 @@ Game.prototype.earnedCredits = function(earn){
   this.credits += earn;  
 };
 
-// THIS AREA ADDRESSES THE GAME
-function Base(){
-    // This value can be changed later on
-    this.health = 100;
-    // Location will be based on map design and the path
-    // this.location
-}
-
-Base.prototype.updateHealth = function(enemDamage){
-    this.health -= enemDamage;  
+Game.prototype.damageBase = function(){
+  this.health -=1;
+  return this.health <= 0;
 };
+
 
