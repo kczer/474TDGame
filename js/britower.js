14f4f2fa2e9c4636e22e4cde5974c9c7a2b7a440
feralislatr <slate@udel.edu>
2016-03-10 13:16:50 +0000

    idk

--- js/britower.js
+++ js/britower.js
@@ -1,4 +1,5 @@
 var $atower;
+var i=1;
  
 $(document).ready(function(){
 
@@ -11,13 +12,14 @@ $(document).ready(function(){
 
         var self = this;
     
-        
-        
         this.drawTower = function(){
-            $atower= $(document.createElement('div')).attr('id', 'tower');
+            $atower= $(document.createElement('div')).attr('id', 'turret');
+            $atower.css("height", "150px");
+            $atower.css("width", "150px");
+            $atower.css("position", "absolute");
             $atower.css("left", this.posx+"px");
             $atower.css("top ", this.posy+"px");
-            $atower.css("background-image", "url('img/tower/sprite_1.png')");
+            $atower.css("background-image", "url('/img/tower/sprite_1.png')");
             
             $('#bg').append($atower);
     
@@ -25,12 +27,13 @@ $(document).ready(function(){
         
         
         this.moveTower = function(){
-            var i=1;
             if(i<=8){
-               $atower.css("background-image", "url('img/tower/sprite_'"+i+".png)");
-               console.log("hi");
-               i++;
-               setTimeout(function(){self.moveTower()},100);
+               var img = "url('/img/tower/sprite_'"+i+"'.png')";
+               $atower.css("background-image", img);
+               console.log($atower.css("background-image"));
+               console.log("hi"+i);
+               ++i;
+               setTimeout(self.moveTower(),100);
                
            }
            else{
@@ -40,9 +43,7 @@ $(document).ready(function(){
              
         }
         
-         
-    
-        
+           
     }
     
     
